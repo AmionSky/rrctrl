4517ed699f521f5eb5e40cebc9d0e8345c01f962
Csányi István <icsanyi96@gmail.com>
2024-10-30 17:08:00 +0100

    Move settings out of this repo

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        const ICON: &str = "../../assets/application.ico";
+        const ICON: &str = "./assets/application.ico";
 
         let mut res = tauri_winres::WindowsResource::new();
         res.set_icon(ICON);
--- crates/rrctrl-settings/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    slint_build::compile("ui/settings-window.slint").unwrap();
-}
